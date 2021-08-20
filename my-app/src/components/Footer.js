c32040875ce968b329e0e8357067b59cecfe7e68
csbonang <csgbonang@gmail.com>
2021-08-20 11:00:05 -0700

    added buttons to footer. todo: add links to platforms

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -4,12 +4,18 @@
 // GitHub and LinkedIn profiles, and their profile on a third platform 
 // (Stack Overflow, Twitter)
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 import '../styles/Footer.css';
+
 
 function Footer() {
     return (
       <div className='footer' > 
-      <p>We're going to place buttons here</p>
+        <div className='ButtonGroup'>
+        <Button> LinkedIn </Button>
+        <Button> GitHub </Button>
+        <Button> Instagram </Button>
+        </div>
       </div> 
     );
   }
