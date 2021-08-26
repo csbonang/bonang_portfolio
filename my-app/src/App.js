f32a7698258fc238c1afdc826198c0a76a7239b4
csbonang <csgbonang@gmail.com>
2021-08-25 18:04:44 -0700

    portfolio card format fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 // import Header from "./components/Header";
 // import Section from "./components/Section";
 import Navbar from "./components/Navbar";
